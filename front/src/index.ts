867207a111aa9222f29bd0d9e29cfd45e8d154ab
RD-961 <ali.raad@epitech.eu>
2023-10-06 01:50:34 +0200

    add Cordova to the project

--- front/src/index.ts
+++ front/src/index.ts
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {configureAxios} from './axiosConfiguration';
+import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+declare let window: any;
 
-configureAxios();
-root.render(React.createElement(App));
+const startApp = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  configureAxios();
+  root.render(React.createElement(App));
+  serviceWorker.register();
+}
+
+if (window.cordova) {
+  document.addEventListener('deviceready', startApp, false);
+} else {
+  startApp();
+}
+
 
