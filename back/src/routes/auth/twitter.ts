d1f0d1659041b49da10c87a7779fd87f6715178e
Timothé MEDICO <medico.timothe@gmail.com>
2023-10-10 02:41:20 +0200

    feat: Setup OAuth for Twitter

--- back/src/routes/auth/twitter.ts
+++ back/src/routes/auth/twitter.ts
@@ -1,43 +1,16 @@
 import {Router, Response, Request} from 'express';
-import axios from 'axios';
-import qs from 'querystring';
 import {isString} from '../../service/authService';
-import oAuth1a from 'twitter-v1-oauth';
+import {requestAccessToken, requestToken, TwitterResponse} from '../../service/twitterService';
 
 const router = Router();
-const CONSUMER_KEY = 'LXhOVDkwR0FYbjBROWloZkRtNDU6MTpjaQ';
-const CONSUMER_SECRET = 'iCrmablHnSyw5_c2ioaw5thb0CKugWzbPfoYNe9jvsqaJ88Oqt';
-const CALLBACK_URL = 'http://localhost:8080/api/auth/twitter/callback';
-
-const oAuthOptions = {
-  api_key: process.env.TWITTER_API_KEY || "HSljaPPH469eKLYOkicfr7ynF",
-  api_secret_key: process.env.TWITTER_API_SECRET_KEY || "8P4ZVXN38OpXIU9q013Gz7ZwgdrzDaGA7PvqCE6noLruIzb9tA",
-  access_token: process.env.TWITTER_ACCESS_TOKEN || "3347689185-vbQnrSQJNo28HLThgsCQKDwwXSHoAv40gZGeKTI",
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET || "7jOfsprUOXfUMwppUNXY4Bzc8xw6L6WXhHAnpaPKxbeHq",
-};
-const url = "https://api.twitter.com/oauth/request_token";
-const method = "POST";
-const params = { oauth_callback: CALLBACK_URL };
-
-const authorization = oAuth1a({ method, url, params }, oAuthOptions);
 
 router.get('/api/auth/twitter', [], async (req: Request, res: Response) => {
   try {
-    const response = await axios.post(url, qs.stringify(params), {
-      headers: {
-        authorization,
-      },
-    });
+    const token: string = await requestToken();
 
-    const oauthData = qs.parse(response.data);
-
-    if (typeof oauthData.oauth_token === 'string') {
-      res.redirect(`https://api.twitter.com/oauth/authenticate?oauth_token=${oauthData.oauth_token}`);
-    } else {
-      throw new Error('Invalid oauth_token');
-    }
+    res.redirect(`https://api.twitter.com/oauth/authenticate?oauth_token=${token}`);
   } catch (error) {
-    console.warn('request', error, error.response.data);
+    console.warn('TWITTER OAUTH', error);
     res.status(500).send('Error initiating Twitter auth');
   }
 });
@@ -48,28 +21,12 @@ router.get('/api/auth/twitter/callback', [], async (req: Request, res: Response)
 
   if (!isString(oauth_token) || !isString(oauth_verifier)) return;
 
-  const request_data = {
-    url: 'https://api.twitter.com/oauth/access_token',
-    method: 'POST',
-    data: {
-      oauth_token,
-      oauth_verifier,
-    },
-  };
-
   try {
-    const response = await axios.post(request_data.url, qs.stringify(request_data.data), {
-      headers: {
-        authorization,
-      },
-    });
+    const response: TwitterResponse = await requestAccessToken(oauth_token, oauth_verifier);
 
-    const authData = qs.parse(response.data);
-    console.log(authData);
-
+    console.log(response);
     res.redirect('/');
   } catch (error) {
-    console.warn('callback', error);
     res.status(500).send('Error during Twitter callback processing');
   }
 });
---
+++ back/src/service/twitterService.ts
@@ -0,0 +1,82 @@
+import {OAuthOptions} from 'twitter-v1-oauth/lib/types';
+import oAuth1a from 'twitter-v1-oauth';
+import axios from 'axios';
+import qs from 'querystring';
+import {isString} from './authService';
+
+interface TwitterResponse {
+  oauth_token: string;
+  oauth_token_secret: string;
+  user_id: string;
+  screen_name: string;
+}
+
+interface TwitterAPICall {
+  url: string;
+  method: 'POST' | 'GET';
+  data: Record<string, string | number | boolean>;
+}
+
+const CALLBACK_URL = 'http://localhost:8080/api/auth/twitter/callback';
+
+async function callTwitterAPI(call: TwitterAPICall, access_token: string) {
+  const authOptions: OAuthOptions = {
+    api_key: process.env.TWITTER_API_KEY || '',
+    api_secret_key: process.env.TWITTER_API_SECRET_KEY || '',
+    access_token: access_token,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET || '',
+  };
+  const {url, method, data} = call;
+  const authorization = oAuth1a({method, url, data}, authOptions);
+
+  return axios.request({
+    url: url,
+    method: method,
+    headers: {authorization},
+    data: qs.stringify(data),
+  });
+}
+
+async function requestToken(): Promise<string> {
+  const url = 'https://api.twitter.com/oauth/request_token';
+
+  return callTwitterAPI({
+    url: url,
+    method: 'POST',
+    data: {
+      oauth_callback: CALLBACK_URL,
+    },
+  }, process.env.TWITTER_ACCESS_TOKEN)
+    .then(response => {
+      const responseData = qs.parse(response.data);
+
+      if (isString(responseData.oauth_token))
+        return responseData.oauth_token;
+      return Promise.reject(new Error('Invalid OAuth Token'));
+    })
+    .catch(reason => {
+      return Promise.reject(new Error('Unable to request token from Twitter:' + reason));
+    });
+}
+
+async function requestAccessToken(token: string, verifier: string): Promise<TwitterResponse> {
+  const url = 'https://api.twitter.com/oauth/access_token';
+
+  return callTwitterAPI({
+    url: url,
+    method: 'POST',
+    data: {
+      oauth_verifier: verifier,
+    },
+  }, token)
+    .then(response => {
+      const responseData: unknown = qs.parse(response.data);
+
+      return responseData as TwitterResponse;
+    }).catch(reason => {
+      return Promise.reject(new Error('Unable to request access token from Twitter:' + reason));
+    });
+}
+
+export {requestToken, requestAccessToken};
+export type {TwitterResponse};
