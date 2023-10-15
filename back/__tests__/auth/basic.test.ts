1576a2c8ee244a99fd21c173ab5c14c2ec64bd9f
Timothé MEDICO <medico.timothe@gmail.com>
2023-10-16 00:45:32 +0200

    feat: Test /auth/register route

--- back/__tests__/auth/basic.test.ts
+++ back/__tests__/auth/basic.test.ts
@@ -4,26 +4,65 @@ import request from 'supertest';
 import {User} from '../../src/model/user';
 import {Credentials, hashPassword} from '../../src/service/authService';
 
+const USER_CREDENTIALS: Credentials = {
+  email: 'user@gmail.com',
+  password: 'password!',
+};
+
+async function saveUser(credentials: Credentials) {
+  const hashedCredentials: Credentials = {
+    ...credentials,
+    password: await hashPassword(credentials.password),
+  };
+
+  await new User(hashedCredentials).save();
+}
+
 describe('Basic Auth Login', () => {
-  const credentials: Credentials = {email: 'user@gmail.com', password: 'pass'};
-
   beforeEach(async () => {
-    const hashedCredentials: Credentials = {...credentials, password: await hashPassword(credentials.password)};
-
-    await new User(hashedCredentials).save();
+    await saveUser(USER_CREDENTIALS);
   });
 
   test('Good Credentials', async () => {
     const response = await request(app).post('/api/auth/login')
-      .send({email: 'user@gmail.com', password: 'pass'});
+      .send(USER_CREDENTIALS);
 
     expect(response.status).toBe(200);
   });
 
   test('Bad Credentials', async () => {
     const response = await request(app).post('/api/auth/login')
-      .send({email: 'user@gmail.com', password: 'passd'});
+      .send({email: USER_CREDENTIALS.email, password: 'bad_password'});
 
     expect(response.status).toBe(401);
+  });
+});
+
+describe('Basic Auth Register', () => {
+  test('Register New User', async () => {
+    const response = await request(app).post('/api/auth/register')
+      .send(USER_CREDENTIALS);
+
+    expect(response.status).toBe(201);
+  });
+
+  test('Register Bad User', async () => {
+    const passwords: string[] = ['pass', '123456.', 'pass@'];
+
+    for (const password of passwords) {
+      const response = await request(app).post('/api/auth/register')
+        .send({...USER_CREDENTIALS, password});
+
+      expect(response.status).toBe(403);
+    }
+  });
+
+  test('Register Existing User', async () => {
+    await saveUser(USER_CREDENTIALS);
+
+    const response = await request(app).post('/api/auth/register')
+      .send(USER_CREDENTIALS);
+
+    expect(response.status).toBe(403);
   });
 });
--- back/src/service/authService.ts
+++ back/src/service/authService.ts
@@ -3,6 +3,10 @@ import {genSalt, hash, compare} from 'bcrypt';
 import {IUser, User} from '../model/user';
 import {generateAccessToken} from '../middleware/auth';
 
+const EMAIL_REGEX: RegExp = /^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/;
+const PASSWORD_CHARACTERS: string = '@?:-_~&=+/*%*^\\!.,;<>"\'()[]{}$';
+const PASSWORD_LENGTH: number = 8;
+
 type Credentials = {
   email: string;
   password: string;
@@ -33,11 +37,25 @@ async function userExists(email: string): Promise<boolean> {
     });
 }
 
+function isValidCredentials(credentials: Credentials): boolean {
+  const email = credentials.email.trim();
+  const password = credentials.password.trim();
+
+  const validEmail: boolean = EMAIL_REGEX.test(email);
+  const validPasswordLength: boolean = password.length >= PASSWORD_LENGTH;
+  const specialCharacters: string[] = password.split('')
+    .filter(character => PASSWORD_CHARACTERS.includes(character));
+
+  return (validEmail && validPasswordLength && specialCharacters.length > 0);
+}
+
 async function register(credentials: Credentials): Promise<IUser> {
   const exists = await userExists(credentials.email);
 
   if (exists)
     return reject('User with this email already exists');
+  if (!isValidCredentials(credentials))
+    return reject('Invalid credentials');
   return saveUser({
     email: credentials.email,
     password: await hashPassword(credentials.password),
