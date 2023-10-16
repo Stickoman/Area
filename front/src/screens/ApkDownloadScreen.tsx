dbf2461b96a597ac353d7fcaacaf38654dee5d26
RD-961 <ali.raad@epitech.eu>
2023-10-16 23:50:18 +0200

    refact: change shared folder path but still have a problem when download, provide an index.html instead of an client.apk

--- front/src/screens/ApkDownloadScreen.tsx
+++ front/src/screens/ApkDownloadScreen.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 function APKDownloadScreen(): React.JSX.Element {
+  const handleDownload = () => {
+    const filePath = '../shared/client.apk';
+    const a = document.createElement('a');
+    a.href = filePath;
+    a.download = 'client.apk';
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
+
   return (
     <div>
-      <p>Downloading the Android version of the mobile client...</p>
-      <a href="../../shared/client.apk" download="client.apk">
-        Click here to download the APK file
-      </a>
+      <button onClick={handleDownload}>Télécharger le fichier</button>
     </div>
   );
 }
