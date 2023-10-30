8484fe2af3fc6219025160da6677521579bb021c
Timothé MEDICO <medico.timothe@gmail.com>
2023-10-30 12:48:14 +0100

    feat: Send message to webhook in case of deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
 
+export WEBHOOK_URL="https://discord.com/api/webhooks/1168515563077775430/pGtOucUOUd9Tl0N_qhrtxaMFBCGqyvuM4s-IQvchN560pNZIrisdiHXdoyMDeoRYpJ_z"
+
+curl \
+  -H "Content-Type: application/json" \
+  -d '{"username": "Deployment Notifier", "content": "Someone started deploying application"}' \
+  $WEBHOOK_URL
+
 eval "$(ssh-agent)" && ssh-add ~/.ssh/id_ed25519
 tar --exclude='node_modules' -czf area.tar.gz ./area
 scp area.tar.gz root@area.baragouin.fr:/root/dev
 ssh root@area.baragouin.fr 'cd /root/dev/ && tar -xzf area.tar.gz && cd area && docker-compose up -d --build front back'
+
+curl \
+  -H "Content-Type: application/json" \
+  -d '{"username": "Deployment Notifier", "content": "Deployment finished!"}' \
+  $WEBHOOK_URL
